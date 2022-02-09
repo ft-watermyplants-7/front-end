e3d69d8d792791cd5f627e4bd9afcbea73142f96
Kristian Fulkerson <kristianf451@gmail.com>
2022-02-08 16:52:53 -0800

    new line test
    Co-authored-by: name <kristianf451@gmail.com>

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
-
+  console.log("test");
   return axios.create({
     headers: {
       authorization: token,
